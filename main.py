76c823d773afd930576d0e0f634b03c7f7e80eab
unknown <zylo117@hotmail.com>
2018-06-28 00:18:05 +0800

    fix list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import time
+import cgitb
+cgitb.enable()
 
 import numpy as np
 
@@ -15,13 +17,13 @@ conf = [c.strip('\n').split(',') for c in conf]
 conf = np.array(conf)
 
 addr_by_sut = []
-for i in range(len(conf[:, ])):
+for i in range(len(conf[:, ]) * 4):
     addr = '\\\\' + conf[:, 1][i // 4] + '\\d$\\EpocyInsp\\EW{}\\{}\\{}\\{}'.format(i % 4 + 1, dt[0], dt[1], dt[2])
     addr_by_sut.append([conf[:, 0][i // 4], i % 4 + 1, addr])
 addr_by_sut = np.array(addr_by_sut)
 
 # start N threads to monitor so many machines and process images
-for addr in addr_by_sut:
+for addr in addr_by_sut[:]:
     fm = FileMonitor(addr[2], 1, 100, 'glue.jpg')
     fm.start()
     print('monitoring {} SUT{}'.format(addr[0], addr[1]))
